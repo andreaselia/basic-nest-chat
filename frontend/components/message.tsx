94c611ac871410ab53a399f1f11142258325e162
AndreasElia <andreaselia@live.co.uk>
2021-10-29 19:10:30 +0100

    update message time position

--- frontend/components/message.tsx
+++ frontend/components/message.tsx
@@ -1,7 +1,11 @@
 function Message() {
-  return <div className="border border-gray-300 rounded-md text-sm p-4">
-    <div>
+  return <div className="bg-gray-100 rounded-md text-sm p-4">
+    <div className="flex justify-between">
       <strong>John Doe</strong>
+
+      <p className="mt-1 text-xs font-semibold text-gray-500">
+        19:10
+      </p>
     </div>
 
     Lorem ipsum dolor
